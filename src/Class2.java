8ca1430b3619a6a47570a066cc62252d68948a4c
christopherhaase <christopherhaa@gmail.com>
2022-02-16 15:57:59 +0100

    Commit 1

---
+++ src/Class2.java
@@ -0,0 +1,6 @@
+public class Class2 {
+
+    public void print() {
+        System.out.println("Some more text");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class2 c = new Class2();
+
+
+        System.out.println("Some text");
+        c.print();
+
+    }
+}
